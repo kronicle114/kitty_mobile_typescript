3eef7e9a2b95af37cf2018104e6d6887cfa49720
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-07-04 13:40:27 -0400

    requiring env to handle stuff

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 3050,
-  JWT_SECRET: process.env.JWT_SECRET || 'secret',
+  JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-  API_BASE_URL: process.env.API_URL || 'http://192.168.1.15:8085',
+  API_BASE_URL: process.env.API_URL || `http://${DEVICE_IP}:8085`,
 }
