006d4bdc45cb21de554a85c8ad934c21b9cea10a
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-07-04 14:02:39 -0400

    fixed cannot find env variables error

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 3050,
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-  API_BASE_URL: process.env.API_URL || `http://${DEVICE_IP}:8085`,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  API_BASE_URL: process.env.API_URL || `http://${process.env.DEVICE_IP}:8085`,
 }
